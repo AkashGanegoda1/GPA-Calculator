4d4ddbcbd28060c0bf486cf53f1b0e2db5f0cdbf
Akash Ganegoda <manidusakithganegoda@gmail.com>
2024-07-04 23:03:16 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GPA_Calculator
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new GPA_Cal_Form());
+        }
+    }
+}
